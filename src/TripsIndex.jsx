129233d365c176094dc215838680e37209819f48
Alcy Torres <mxalcytorres@gmail.com>
2024-08-04 00:42:14 -0400

    Trips index action

---
+++ src/TripsIndex.jsx
@@ -0,0 +1,20 @@
+export function TripsIndex(props) {
+  return (
+    <div>
+      <h1>All trips</h1>
+      {props.trips.map((trip) => (
+        <div key={trip.id}>
+          <h2>{trip.location}</h2>
+          <img src={trip.image_url} />
+          <p>Country: {trip.country}</p>
+          <p>Continent: {trip.continent}</p>
+          <p>Year: {trip.year}</p>
+          <p>Latitude: {trip.latitude}</p>
+          <p>Longitude: {trip.longitude}</p>
+          <p>Highlights: {trip.highlights}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
