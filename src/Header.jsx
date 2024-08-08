a52fce91944cf6eaa7af72eac386345acd54c38c
Alcy Torres <mxalcytorres@gmail.com>
2024-08-08 17:45:02 -0400

    Link to add a new trip from main page

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header>
       <nav>
-        <a href="/">Home</a> | <a href="/signup">Signup</a> | <a href="/login">Login</a> | <LogoutLink /> | <a href="/user_trips">My Trips</a> 
+        <a href="/">Home</a> | <a href="/signup">Signup</a> | <a href="/login">Login</a> | <LogoutLink /> | <a href="/user_trips">My Trips</a> | <a href="/trips/new">Add a trip</a> 
       </nav>
     </header>
   )
