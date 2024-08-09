fae9419dd92840ee7b8b59394b71bbd09a3b5dd2
Alcy Torres <mxalcytorres@gmail.com>
2024-08-09 16:58:08 -0400

    Bootstrap for login

--- src/Login.jsx
+++ src/Login.jsx
@@ -37,14 +37,32 @@ export function Login() {
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
-        <div>
-          Email: <input name="email" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <button type="submit">Login</button>
+
+        <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="email" name="email" />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="password" className="form-control" id="password" name="password" />
+          </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
   );
 }
+
+      // <form onSubmit={handleSubmit}>    
+      //   <div>
+      //     Email: <input name="email" type="email" />
+      //   </div>
+      //   <div>
+      //     Password: <input name="password" type="password" />
+      //   </div>
+      //   <button type="submit">Login</button>
+      // </form>
+
+
+     
